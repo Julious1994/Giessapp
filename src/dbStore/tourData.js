038043092f4ae05c59af039b12aa7ebab05621d8
Amit Patel <mtpatel557@yahoo.com>
2018-11-13 20:00:10 +0530

    add dummy tour data

---
+++ src/dbStore/tourData.js
@@ -0,0 +1,68 @@
+export const tours = [
+    {
+        name: 'Tour 1',
+        id: '1',
+        createdAt: '08-11-2018',
+        completedWorkItems: [],
+    },
+    {
+        name: 'Tour 2',
+        id: '2',
+        createdAt: '12-02-2018',
+        completedWorkItems: [],
+    },
+    {
+        name: 'Tour 3',
+        id: '3',
+        createdAt: '01-01-2018',
+        completedWorkItems: [],
+    },
+    {
+        name: 'Tour 4',
+        id: '4',
+        createdAt: '25-08-2017',
+        completedWorkItems: [],
+    },
+    {
+        name: 'Tour 5',
+        id: '5',
+        createdAt: '25-08-2017',
+        completedWorkItems: [],
+    },
+    {
+        name: 'Tour 6',
+        id: '6',
+        createdAt: '25-08-2016',
+        completedWorkItems: [],
+    },
+    {
+        name: 'Tour 7',
+        id: '7',
+        createdAt: '25-08-2015',
+        completedWorkItems: [],
+    },
+    {
+        name: 'Tour 8',
+        id: '8',
+        createdAt: '25-08-2016',
+        completedWorkItems: [],
+    },
+    {
+        name: 'Tour 9',
+        id: '9',
+        createdAt: '25-08-2015',
+        completedWorkItems: [],
+    },
+    {
+        name: 'Tour 10',
+        id: '10',
+        createdAt: '25-08-2016',
+        completedWorkItems: [],
+    },
+    {
+        name: 'Tour 11',
+        id: '11',
+        createdAt: '25-08-2015',
+        completedWorkItems: [],
+    }
+]
