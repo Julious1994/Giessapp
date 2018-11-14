95a7997be10c0fac9707c816b371d07e87fe0f78
Amit Patel <mtpatel557@yahoo.com>
2018-11-14 22:50:56 +0530

    configure cached image plugin

--- android/app/src/main/java/com/giessapp/MainApplication.java
+++ android/app/src/main/java/com/giessapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.giessapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.rnfs.RNFSPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
             new RNFSPackage(),
             new OrientationPackage(),
             new VectorIconsPackage()
